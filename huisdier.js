3504d5d85e4432f30106ff17e2b08bca5c0a5ce7
Thimen-Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-06-02 11:28:50 +0200

    v2

--- huisdier.js
+++ huisdier.js
@@ -1,7 +1,9 @@
 function Huisdier(dier, naam, leeftijd) {
   (this.dier = dier), (this.naam = naam), (this.leeftijd = leeftijd);
-  infoDier = `mijn ${this.dier}, ${this.naam} is ${this.leeftijd} jaar oud`;
-  return infoDier;
+}
+
+function print() {
+  return `mijn ${this.dier} ${this.naam}, is ${leeftijd} jaar oud`;
 }
 
 module.exports = { Huisdier };
